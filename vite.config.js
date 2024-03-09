2780cf83344f174341db7f9b972b79be5d10507f
Yakshith Salian <144924386+Yakshith966@users.noreply.github.com>
2024-03-09 15:42:29 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,4 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  css:{
-    modules:{
-      localsConvention:"camelCase"
-    }
-  }
-})
+
